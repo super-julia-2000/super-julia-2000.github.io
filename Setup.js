53a137f301231de1d5887d424e78cffeb5c3eb99
super-julia-2000 <edu.alliance.nj@yandex.ru>
2025-02-09 19:53:56 +0300

    hj

--- Setup.js
+++ Setup.js
@@ -1,8 +1,39 @@
 document.getElementById('Main').hidden = true;
 document.getElementById('Clients').hidden = false;
-console.log("hello everynyan");
-function onFocusTel(){
-    console.log("jjk");
+function InputTel(caller) {
+
+}
+function outFocusTel(caller) {
+    if (caller.value.length == 0)
+        caller.value = "+7";
+    else {
+        var count = 0;
+        if (caller.value.length == 1) {
+            count = 2;
+            if (caller.value.charAt(0) == '+' || caller.value.charAt(0) == '7')
+                count--;
+        } else {
+            if (caller.value.charAt(0) == '+') {
+                if (caller.value.charAt(1) != '7')
+                    count++;
+            } else {
+                count++;
+                if (caller.value.charAt(0) != '7')
+                    count++;
+            }
+        }
+        if (count != 0) {
+            var arr = new Array(caller.value.length + count);
+            var ind = 0;
+            if (count == 1)
+                ind = 1;
+            arr[0] = '+';
+            arr[1] = '7';
+            for (var i = 2; i < arr.length; i++, ind++)
+                arr[i] = caller.value.charAt(ind);
+            caller.value = arr.flat().join('');
+        }
+    }
 }
 /*const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
     "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
