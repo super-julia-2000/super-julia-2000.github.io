0ca99c033a799d5d610c0fb035d71e26115d5843
super-julia-2000 <edu.alliance.nj@yandex.ru>
2025-02-12 12:43:41 +0300

    hjkl

---
+++ scripts/reference_book_scripts/modal_handler.js
@@ -0,0 +1,6 @@
+function create_group(){
+    document.getElementById("form_create_group").style.display="block";
+}
+function close_group(){
+    document.getElementById("form_create_group").style.display="none";
+}
