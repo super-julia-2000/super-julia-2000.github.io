c9c61862101fbc3f9239b601777dc7eafb164a35
super-julia-2000 <edu.alliance.nj@yandex.ru>
2025-02-13 04:41:16 +0300

    hjk

--- scripts/reference_book_scripts/modal_handler.js
+++ scripts/reference_book_scripts/modal_handler.js
@@ -1,12 +1,21 @@
-var modal = document.getElementById("form_create_group");
-function create_group(){
-    document.getElementById("form_create_group").style.display="block";
+var form_create_group = document.getElementById("form_create_group");
+var form_create_list = document.getElementById("form_create_list");
+function create_group() {
+    form_create_group.style.display = "block";
 }
-function close_group_creater(){
-    document.getElementById("form_create_group").style.display="none";
+function close_group_creater() {
+    form_create_group.style.display = "none";
 }
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+function create_list() {
+    form_create_list.style.display = "block";
+}
+function close_list_creater(){
+    form_create_list.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == form_create_group) {
+        close_group_creater()
+    } else if(event.target == form_create_list){
+        close_list_creater()
     }
-  }
+}
